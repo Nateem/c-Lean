32e8ebcc6a1bff320ae5fe360dcb16aec974f6b6
Natee <nateeteem@gmail.com>
2018-01-30 21:30:23 +0700

    add test01

---
+++ test01/test01/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test01
+{
+    class Class1
+    {
+        int  loop;
+        int m;
+        public Class1(string max)
+        {
+            //Console.WriteLine(max);
+            int.TryParse(max , out m);
+            //Console.WriteLine(m);
+            this.loop = m;
+            Init(m);
+        }
+        public void Init(int max) {
+            int i = 0;
+            int x = 0;
+            do
+            {
+
+                if (i == max)
+                {
+
+                    Console.WriteLine($" {i - x} \n");
+                    i = x;
+                    x++;
+                }
+                else
+                {
+                    Console.Write($" {i - x}");
+                }
+                i++;
+            }
+            while ( i <= max);
+            
+        }
+    }
+}
---
+++ test01/test01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Number : ");
+            string a = Console.ReadLine();
+            Class1 c1 = new Class1(a);
+            Console.ReadKey();
+        }
+    }
+}
